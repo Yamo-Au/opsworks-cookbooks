d5554d9f5e1b25e55a61b66d366634df7cc30639
Matt Taylor <mattctaylor@users.noreply.github.com>
2014-09-03 11:02:31 +1000

    Update default.rb

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -10,7 +10,7 @@ default['wordpress']['wp_config']['force_secure_logins'] = false
 # Deploy applications to EBS volume
 default[:deploy] = {}
 node[:deploy].each do |application, deploy|
-  default[:deploy][application][:deploy_to] = "/srv/www/#{application}"
+  default[:deploy][application][:deploy_to] = "/mnt/www/#{application}"
   default[:deploy][application][:chef_provider] = node[:deploy][application][:chef_provider] ? node[:deploy][application][:chef_provider] : node[:opsworks][:deploy_chef_provider]
   unless valid_deploy_chef_providers.include?(node[:deploy][application][:chef_provider])
     raise "Invalid chef_provider '#{node[:deploy][application][:chef_provider]}' for app '#{application}'. Valid providers: #{valid_deploy_chef_providers.join(', ')}."
