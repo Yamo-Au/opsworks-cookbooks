ba3b0665fee8422499ac0b56d852b37acebdbbd2
Matt Taylor <mattctaylor@users.noreply.github.com>
2014-09-03 12:11:23 +1000

    Update deploy.rb

--- wordpress/recipes/deploy.rb
+++ wordpress/recipes/deploy.rb
@@ -3,13 +3,9 @@
 # Recipe:: wordpress
 #
 
-include_recipe 'deploy'
+include_recipe 'php'
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'php'
-    Chef::Log.debug("Skipping deploy::php application #{application} as it is not an PHP app")
-    next
-  end
 
   opsworks_deploy_dir do
     path deploy[:deploy_to]
