2c48f925e6410b5405c51f588f171b16409d2a22
Matt Taylor <mattctaylor@users.noreply.github.com>
2014-09-03 11:14:54 +1000

    Update default.rb

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -24,3 +24,51 @@ node[:deploy].each do |application, deploy|
   else
     default[:deploy][application][:absolute_document_root] = "#{default[:deploy][application][:current_path]}/"
   end
+
+  if File.exists?('/usr/local/bin/rake')
+    # local Ruby rake is installed
+    default[:deploy][application][:rake] = '/usr/local/bin/rake'
+  else
+    # use default Rake/ruby
+    default[:deploy][application][:rake] = 'rake'
+  end
+
+  default[:deploy][application][:migrate] = false
+
+  if node[:deploy][application][:auto_bundle_on_deploy]
+    default[:deploy][application][:migrate_command] = "if [ -f Gemfile ]; then echo 'OpsWorks: Gemfile found - running migration with bundle exec' && /usr/local/bin/bundle exec #{node[:deploy][application][:rake]} db:migrate; else echo 'OpsWorks: no Gemfile - running plain migrations' && #{node[:deploy][application][:rake]} db:migrate; fi"
+  else
+    default[:deploy][application][:migrate_command] = "#{node[:deploy][application][:rake]} db:migrate"
+  end
+  default[:deploy][application][:rails_env] = 'production'
+  default[:deploy][application][:action] = 'deploy'
+  default[:deploy][application][:user] = node[:opsworks][:deploy_user][:user]
+  default[:deploy][application][:group] = node[:opsworks][:deploy_user][:group]
+  default[:deploy][application][:shell] = node[:opsworks][:deploy_user][:shell]
+  default[:deploy][application][:home] = if !node[:opsworks][:deploy_user][:home].nil?
+                                           node[:opsworks][:deploy_user][:home]
+                                         elsif self[:passwd] && self[:passwd][self[:deploy][application][:user]] && self[:passwd][self[:deploy][application][:user]][:dir]
+                                           self[:passwd][self[:deploy][application][:user]][:dir]
+                                         else
+                                           "/home/#{self[:deploy][application][:user]}"
+                                         end
+  default[:deploy][application][:sleep_before_restart] = 0
+  default[:deploy][application][:stack][:needs_reload] = true
+  default[:deploy][application][:enable_submodules] = true
+  default[:deploy][application][:shallow_clone] = false
+  default[:deploy][application][:delete_cached_copy] = true
+  default[:deploy][application][:create_dirs_before_symlink] = ['tmp', 'public', 'config']
+  default[:deploy][application][:symlink_before_migrate] = {}
+
+  default[:deploy][application][:environment] = {"RAILS_ENV" => deploy[:rails_env],
+                                                 "RUBYOPT" => "",
+                                                 "RACK_ENV" => deploy[:rails_env],
+                                                 "HOME" => node[:deploy][application][:home]}
+  default[:deploy][application][:ssl_support] = false
+  default[:deploy][application][:auto_npm_install_on_deploy] = true
+
+  # nodejs
+  default[:deploy][application][:nodejs][:restart_command] = "monit restart node_web_app_#{application}"
+  default[:deploy][application][:nodejs][:stop_command] = "monit stop node_web_app_#{application}"
+  default[:deploy][application][:nodejs][:port] = deploy[:ssl_support] ? 443 : 80
+end
