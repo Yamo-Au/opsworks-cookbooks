04aced2e8e2cd8b6f64a24b341b9a05b2e4f51a1
mattctaylor <mattctaylor@Mattbook-Pro.local>
2014-09-03 14:56:40 +1000

    revert back to baseline

--- wordpress/attributes/default.rb
+++ wordpress/attributes/default.rb
@@ -6,9 +6,3 @@ default['wordpress']['wp_config']['enable_W3TC'] = false
 
 # Force logins via https (http://codex.wordpress.org/Administration_Over_SSL#To_Force_SSL_Logins_and_SSL_Admin_Access)
 default['wordpress']['wp_config']['force_secure_logins'] = false
-
-# Deploy applications to EBS volume
-default[:deploy] = {}
-node[:deploy].each do |application, deploy|
-  default[:deploy][application][:deploy_to] = "/mnt/www/#{application}"
-end
